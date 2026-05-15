c479c91e992f18f7002267703c913b4812e9714a
alnamshanmohammed204-art <alnamshanmohammed204@gmail.com>
2026-05-15 14:09:00 +0300

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Developed by Mohammed Al-Namshan, Student ID: [Your Student ID]</p>
+</footer>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,8 @@
+
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="information.php">Information</a></li>
+        <li><a href="reviews.php">Reviews</a></li>
+    </ul>
+</nav>
